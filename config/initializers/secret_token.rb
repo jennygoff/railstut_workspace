0fc75a4ae7be9e65eaaebb14bc5bb989acb89258
Jenny Goff <jennyg@effectivephilanthropy.org>
2013-05-22 10:07:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailstutWorkspace::Application.config.secret_token = 'db8a438f0571d96f5c5ed96e2b5f88b5bfef3f231748606a738407008fbac9d9157efb46639a7cb5397b49882f84e0175ff22f98ed38f74cb202435b71936180'
